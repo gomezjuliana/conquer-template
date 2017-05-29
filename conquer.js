83b56c8c12628f3bd208d9203c4b49e1ddff65b0
Jules <jgmz.jg@gmail.com>
2017-05-29 18:31:52 -0500

    work on scroll functions

--- conquer.js
+++ conquer.js
@@ -1,7 +1,14 @@
-let menuImg = document.querySelector('.hamburger');
-let nav = document.querySelector('.nav');
-menuImg.addEventListener('click', openMenu);
+// This opens and closes the menu with a click on screens less than 900px
+document.querySelector('.hamburger').addEventListener('click', openMenu);
 
 function openMenu() {
-	nav.classList.toggle('nav-open');
+	document.querySelector('.nav').classList.toggle('nav-open');
 };
+
+// This changes the menu on scroll
+
+document.querySelector('.col-3').addEventListener('onscroll', changeMenu);
+
+function changeMenu() {
+	console.log("you're here!");
+}
