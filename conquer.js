c1af91cb7b6b52060694cedff0cec8a24def508e
Jules <jgmz.jg@gmail.com>
2017-05-30 11:25:08 -0500

    try out scroll function

--- conquer.js
+++ conquer.js
@@ -6,9 +6,45 @@ function openMenu() {
 };
 
 // This changes the menu on scroll
-
-document.querySelector('.col-3').addEventListener('onscroll', changeMenu);
-
-function changeMenu() {
-	console.log("you're here!");
+function canISeeDiv(element, showfn, hidefn){
+	let isshown = false;
+	function check () {
+		if (match(pageRectangle(), elementRectangle(element)) !== isshown) {
+			isshown = !isshown;
+			isshown? showfn() : hidefn();
+		}
+	};
+	window.onscroll=window.onresize = check;
+	check();
 }
+
+function pageRectangle() {
+	let page = document.body;
+	let y = page.scrollTop;
+	let h = 'innerHeight' in window? window.innerHeight : page.clientHeight;
+	return [y, h+y];
+}
+
+function elementRectangle(element) {
+	let y = 0;
+	let h = element.offsetHeight;
+	while (element.offsetParent!==null){
+		y+= element.offsetTop;
+		element= element.offsetParent;
+	}
+	return [y, y+h];
+}
+
+function match(a,b){
+	return a[1]<b[3] && a[3]>b[1];
+}
+
+canISeeDiv(
+	document.getElementById('services'),
+	function(){
+		alert('div in view!');
+	},
+	function(){
+		alert('div gone away!');
+	}
+	);
